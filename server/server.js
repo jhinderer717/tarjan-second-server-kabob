7800cf3979e785970a39b839f949118b23dc52c9
John Hinderer <john.hinderer16@gmail.com>
2020-09-02 13:37:03 -0500

    fix missing static incantation

--- server/server.js
+++ server/server.js
@@ -8,6 +8,34 @@ const app = express();
 // Serve static files from server/public folder
 app.use(express.static('server/public'));
 
+// Define a list of activities for my kid
+let activities = [
+    {
+        activity: "Typing practice",
+        isScreentime: true,
+        type: "Mind Exercises"
+    },
+    {
+        activity: "Bike around the lake",
+        isScreentime: false,
+        type: "Body Break"
+    },
+    {
+        activity: "FB chat with a friend",
+        isScreentime: true,
+        type: "Social"
+    }
+];
+
+// Endpoint:
+// GET /activities
+// Respond with a list of activities
+app.get('/activities', function(req, res) {
+    // Send back the array of activities
+    res.send(activities);
+});
+
+
 // Listen for requests
 const port = 3000;
 app.listen(port, function() {
