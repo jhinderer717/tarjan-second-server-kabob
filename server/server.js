0908c2751385d7044a4e99167d8bdcc5db5cb986
John Hinderer <john.hinderer16@gmail.com>
2020-09-02 13:23:39 -0500

    fix missing static incantation

--- server/server.js
+++ server/server.js
@@ -5,8 +5,10 @@ console.log('Look I am a server!');
 // Create an express app
 const app = express();
 
+// Serve static files from server/public folder
+app.use(express.static('server/public'));
 
-// listen for requests
+// Listen for requests
 const port = 3000;
 app.listen(port, function() {
     console.log("This is Dr. Mantis Toboggan");
